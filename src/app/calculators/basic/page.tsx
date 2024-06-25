cf93e6f6e31d152504b0f80f582cb1c4ee574900
Rudra <personal@rudraroy.me>
2024-06-25 09:47:01 +0530

    added title and description

--- src/app/calculators/basic/page.tsx
+++ src/app/calculators/basic/page.tsx
@@ -1,6 +1,12 @@
 import { Metadata } from "next"
 import BasicCalculator from "@/calculators/basic/BasicCalculator"
 
+export const metadata: Metadata = {
+  title: "Basic Calculator",
+  description:
+    "Basic 4 function calculator. This calculator include all arithmetic operations including plus minus and modulus operations.",
+}
+
 export default function Page() {
   return (
     <>
